a75b977d8f7c167ba4497b8c40cd744b202828a9
nhee0002 <89779248+nhee0002@users.noreply.github.com>
2022-10-12 00:30:10 +1100

    Create vega_lite_vis.js

---
+++ vega_lite_vis.js
@@ -0,0 +1,23 @@
+var vg_1 = "json/week9_map.vg.json";
+vegaEmbed("#week9_map", vg_1, {"actions": false}).then(function(result) {
+}).catch(console.error);
+
+var vg_B = "json/production_stacked_area.vg.json";
+vegaEmbed("#prod_stacked_area", vg_B, {"actions": false}).then(function(result) {
+}).catch(console.error);
+
+var vg_F = "json/import_bar.vg.json";
+vegaEmbed("#import_bar", vg_F, {"actions": false}).then(function(result) {
+}).catch(console.error);
+
+var vg_3 = "json/top_export_bar.vg.json";
+vegaEmbed("#export_bar", vg_3, {"actions": false}).then(function(result) {
+}).catch(console.error);
+
+var pie_chart = "json/pie.vg.json";
+vegaEmbed("#pie_chart", pie_chart, {"actions": false}).then(function(result) {
+}).catch(console.error);
+
+var vg_radar1 = "json/radar_arabica.vg.json";
+vegaEmbed("#radar_map1", vg_radar1, {"actions": false}).then(function(result) {
+}).catch(console.error);
